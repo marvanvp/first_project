48d777878de757f70981548cb665474723580ae4
marva <marvanvp6@gmail.com>
2023-03-27 10:45:30 +0530

    Initial commit

---
+++ bin/first_project.dart
@@ -0,0 +1,19 @@
+void main(){
+  ///variable declaration
+  ///datatype variable_name = value;
+  String name = "Marvan";
+  int age     = 23;
+  int phone   = 6282471884;
+  double mark = 7;
+  String email= "marvanvp6@gmail.com";
+
+  print("students details");
+  print("my name is $name");
+  print("im $age yrs old");
+  print("My phone number is $phone");
+  print("CGPA is $mark");
+  print("My email id is $email");
+
+
+
+}
